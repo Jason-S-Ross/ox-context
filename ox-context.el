3615967bf5c8412cead5f7203612bf5238422498
Jason Ross <jasonross1024@gmail.com>
2021-06-08 12:13:32 -0700

    Update todo

--- ox-context.el
+++ ox-context.el
@@ -2514,7 +2514,6 @@ INFO is a plist containing contextual information."
   "Transcode an ITEM element from Org to ConTeXt.
 CONTENTS is the contents of the item. INFO is a plist containing
 contextual information."
-  ;; TODO Handle alphabetical items where `org-list-allow-alphabetical' is set
   (let ((tag (let ((tag (org-element-property :tag item)))
                (and tag (org-export-data tag info))))
         (checkbox (cl-case (org-element-property :checkbox item)
